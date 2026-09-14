821bb51d237c7e137880b639e6a3ebc46a60c9b3
pradeepsambamurthy <pradeep.sambamurthy@gmail.com>
2026-09-14 15:12:03 -0400

    Optimize PeekVision web bundle and remove unused services

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-import 'firebase_options.dart';
 import 'screens/app_shell.dart';
 import 'screens/capture_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const VisionApp());
+  
 }
 
 class VisionApp extends StatelessWidget {
